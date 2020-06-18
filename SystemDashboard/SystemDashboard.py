2aefc17408713b5506e04ab5d558a400d74b2659
rryderr118 <ryder6262@gmail.com>
2020-06-17 22:10:20 -0500

    Update SystemDashboard.py

--- SystemDashboard/SystemDashboard.py
+++ SystemDashboard/SystemDashboard.py
@@ -7,6 +7,7 @@ import tkinter as tk
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from datetime import datetime, timedelta
 from tkcalendar import DateEntry
@@ -19,8 +20,8 @@ class Dashboard:
         self.root = tk.Tk()
         self.frame = tk.Frame(self.root)
         self.createGraphic()
-        self.loadData(os.path.dirname(os.path.realpath(__file__)) + '\modes.xlsx', "Modes", 1, None, None)
-        self.loadData(os.path.dirname(os.path.realpath(__file__)) + '\sites.xlsx', "Sites", 2, None, None)
+        self.loadData(os.path.dirname(os.path.realpath(__file__)) + '\VIPR_DELTA_STATS.xlsx', None, None)
+        self.loadData(os.path.dirname(os.path.realpath(__file__)) + '\VIPR_DELTA_STATS.xlsx', None, None)
         self.loadStatusData()
         self.root.configure(background = 'white')
         self.frame.config(background = 'white')
@@ -45,58 +46,94 @@ class Dashboard:
         self.inWindow.display()
         
     def loadNewData(self, inputWindow):
-        self.loadData(inputWindow.file_path, inputWindow.typeData, inputWindow.columnNum, inputWindow.startDate, inputWindow.endDate)
+        self.loadData(inputWindow.file_path, inputWindow.startDate, inputWindow.endDate)
         
     def openFile(self):
         file_path, dataType, startDate, endDate = self.inWindow.getData()
         columnNum = 1
         self.loadData(file_path, dataType, columnNum, startDate, endDate)
         
-    def loadData(self, filePath, name, columnNum, startDate, endDate):
+    def loadData(self, filePath, startDate, endDate):
         xl = pd.ExcelFile(filePath)
         dfs = {sheet: xl.parse(sheet) for sheet in xl.sheet_names} 
-        preX = dfs['Sheet1']['date'].unique()
-        x = []
-        y = []
+        modesX = []
+        modesY = []
+        sitesX = []
+        sitesY = []
         
         #Add all days to X between start date and end date
+        appendAllDates = False
         if startDate != None and endDate != None:
             delta = endDate - startDate
             for i in range(delta.days + 1):
                 day = startDate + timedelta(days=i)
-                x.append(day)
+                modesX.append(day)
+                sitesX.append(day)
         else:
-            x = preX
+            appendAllDates = True
             
-        #display a pending count
-        pendingCount = 0
-        for i in range(len(x)):
-            y.append(0)
-            for j in dfs['Sheet1']['date']:
-                if j == x[i]:
-                    y[i] += 1
-            for k in dfs['Sheet1']['state']:
-                if k == 'PENDING':
-                    pendingCount += 1
-                    
+        if appendAllDates:
+            for i in range(len(dfs['Sheet1']['COMMENTS'])):
+                comment = dfs['Sheet1']['COMMENTS'][i]
+                date = dfs['Sheet1']['END_DATE'][i]
+                if comment == 'DATAMINER':
+                    modesY.append(dfs['Sheet1']['NUM_NEW_INTS'][i])
+                    modesX.append(date)
+                if comment == 'GEOMINER':
+                    sitesY.append(dfs['Sheet1']['NUM_NEW_INTS'][i])
+                    sitesX.append(date)
+        else:
+            #Add all days to X between start date and end 
+            delta = endDate - startDate
+            for i in range(delta.days + 1):
+                day = startDate + timedelta(days=i)
+                modesX.append(day)
+                sitesX.append(day)
+            for i in range(len(modesX)):
+                for j in range(len(dfs['Sheet1']['date'])):
+                    date = dfs['Sheet1']['END_DATE'][j]
+                    if date == modesX[i]:
+                        comment = dfs['Sheet1']['COMMENTS'][i]
+                        if comment == 'DATAMINER':
+                            modesY.append(dfs['Sheet1']['NUM_NEW_INTS'][j])
+                        if comment == 'GEOMINER':
+                            sitesY.append(dfs['Sheet1']['NUM_NEW_INTS'][j])
+                
         fig, ax = plt.subplots(figsize=(8, 6))
-        plt.yticks(range(0,max(y)+1))
+        plt.yticks(range(0,max(modesY)+1))
         ax.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=0))
         ax.xaxis.set_minor_locator(mdates.DayLocator())
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
-        plt.ylabel("Num " + name)
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(5))
+        ax.yaxis.set_minor_locator(ticker.AutoMinorLocator())
+        plt.ylabel("Num Modes")
         plt.xlabel("Run Date")
         ax.set_facecolor('xkcd:light grey')
-        ax.plot(x,y)
+        ax.plot(modesX,modesY)
         ax.autoscale(enable=True) 
         fig.autofmt_xdate()
-        fig.suptitle(name)
+        fig.suptitle("Modes")
         canvas = FigureCanvasTkAgg(fig, master=self.root)
         canvas.draw()
-        canvas.get_tk_widget().grid(row=1, column=columnNum, rowspan=7, sticky='s')
+        canvas.get_tk_widget().grid(row=1, column=1, rowspan=7, sticky='s')
         
-        label = tk.Label(self.root, text="Pending " + name + ": " + str(pendingCount), fg = "black", bg = "white", font = "Verdana 10")
-        label.grid(row=4)
+        fig, ax = plt.subplots(figsize=(8, 6))
+        plt.yticks(range(0,max(sitesY)+1))
+        ax.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=0))
+        ax.xaxis.set_minor_locator(mdates.DayLocator())
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(5))
+        ax.yaxis.set_minor_locator(ticker.AutoMinorLocator())
+        plt.ylabel("Num Sites")
+        plt.xlabel("Run Date")
+        ax.set_facecolor('xkcd:light grey')
+        ax.plot(sitesX,sitesY)
+        ax.autoscale(enable=True) 
+        fig.autofmt_xdate()
+        fig.suptitle("Sites")
+        canvas = FigureCanvasTkAgg(fig, master=self.root)
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=1, column=2, rowspan=7, sticky='s')
         
     def loadStatusData(self):
         f = open(os.path.dirname(os.path.realpath(__file__)) + "\system.txt", "r")
@@ -115,6 +152,7 @@ class Dashboard:
         label.grid(row=2)
         f.close()
         
+        
 class inputWindow:
     startDate = datetime.today()
     endDate = datetime.today()
@@ -122,7 +160,6 @@ class inputWindow:
     def display(self):
         self.root = tk.Tk()
         frame = tk.Frame(self.root)
-        self.typePicker = ttk.Combobox(self.root,state='readonly', values=("Modes","Sites"))
         self.startCal=DateEntry(self.root,dateformat=3,width=12, background='white',
                     foreground='white', borderwidth=4,Calendar=datetime.now().year)
         self.endCal=DateEntry(self.root,dateformat=3,width=12, background='white',
@@ -137,44 +174,33 @@ class inputWindow:
         tk.Grid.rowconfigure(self.root, 5, weight=1)
         frame.grid(sticky='NSEW', column=0, row=0)
         
-        button.grid(row=3, columnspan=2)
-        label = tk.Label(self.root, text="Type:", fg = "black", bg = "white", font = "Verdana 10")
-        label.grid(row=0, column=0)
-        self.typePicker.grid(row=0, column=1)
+        button.grid(row=2, columnspan=2)
         label = tk.Label(self.root, text="Start Date:", fg = "black", bg = "white", font = "Verdana 10")
-        label.grid(row=1, column=0)
-        self.startCal.grid(row=1, column=1)
+        label.grid(row=0, column=0)
+        self.startCal.grid(row=0, column=1)
         label = tk.Label(self.root, text="End Date:", fg = "black", bg = "white", font = "Verdana 10")
-        label.grid(row=2, column=0)
-        self.endCal.grid(row=2, column=1)
+        label.grid(row=1, column=0)
+        self.endCal.grid(row=1, column=1)
         tk.mainloop()
         
     def getFile(self):
-        if self.typePicker.selection_present():
-            if self.startCal.get_date() < self.endCal.get_date():
-                self.file_path = tk.filedialog.askopenfilename()
-                self.root.focus_force()
-                if self.file_path != "":
-                    self.setData()
-            else:
-                tk.messagebox.showinfo("Input Error", "Start date is not before end date")
-                self.root.focus_force()
+        if self.startCal.get_date() < self.endCal.get_date():
+            self.file_path = tk.filedialog.askopenfilename()
+            self.root.focus_force()
+            if self.file_path != "":
+                self.setData()
         else:
-            tk.messagebox.showinfo("Input Error", "Please input a data type")
+            tk.messagebox.showinfo("Input Error", "Start date is not before end date")
             self.root.focus_force()
             
     def setData(self):
-        self.typeData = self.typePicker.get()
-        self.columnNum = 1
-        if self.typeData == 'Sites':
-            self.columnNum = 2
         self.startDate = self.startCal.get_date()
         self.endDate = self.endCal.get_date()
         returnData()
         self.root.destroy()
         
     def getData(self):
-        return self.file_path, self.typeData, self.columnNum, self.startDate, self.endDate
+        return self.file_path, self.startDate, self.endDate
     
 dash = Dashboard()
 
